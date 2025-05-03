1e0cd4fac549dbaed818c8a7c1d9b6462a43cc93
sujal_01 <sujalpatelyo1042@gmail.com>
2025-05-03 18:56:02 +0530

    initialize dashboard

---
+++ src/app/dashboard/[id]/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import Dashboard from '@/app/components/Dashboard';
+import TaskInput from '@/app/components/TaskInput';
+import { useParams } from 'next/navigation'
+import React from 'react'
+
+export default function page() {
+    const params = useParams()
+    const userId = params.id as string;
+
+    return (
+        <div>
+            <Dashboard id={userId} />
+        </div>
+    )
+}
