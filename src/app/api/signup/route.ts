14e350dbdc0075a58dff84a20e70f7688f91c236
sujal_01 <sujalpatelyo1042@gmail.com>
2025-05-03 14:59:40 +0530

    improve the post logics

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,24 +2,23 @@ import connectDb from "@/app/lib/db";
 import User from "@/app/modules/user";
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function POST(req: NextRequest) {
     try {
-        const { formData } = await req.json();
-        const { username, password } = formData
+        await connectDb();
+        const { formdata } = await req.json();
+        const { username, password } = formdata;
 
-        if (username && password) {
-            await User.create({ username, password }).then((res) => {
-                NextResponse.json({ msg: "SignUp Successfully", signup: true })
-            })
-        } else {
-            NextResponse.json({ msg: 'Failed To SignUp', signup: false })
-        }
+        console.log("username and password in server side", username, password);
+
         if (!username || !password) {
-            NextResponse.json({ msg: 'Invaild Creadential', signup: false })
+            return NextResponse.json({ msg: "Invalid Credentials", signup: false });
         }
+
+        await User.create({ username, password });
+        return NextResponse.json({ msg: "SignUp Successful", signup: true });
 
     } catch (err) {
-        console.log("Error In Login POST API : ", err)
+        console.log("Error In Signup POST API:", err);
+        return NextResponse.json({ msg: "SignUp Failed", signup: false });
     }
 }
