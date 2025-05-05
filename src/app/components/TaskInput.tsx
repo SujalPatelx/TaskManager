817c0267681124c84dd5d12ea4c1c1e2d85ed87b
sujal_01 <sujalpatelyo1042@gmail.com>
2025-05-05 16:29:25 +0530

    sending task data to the dashboard

--- src/app/components/TaskInput.tsx
+++ src/app/components/TaskInput.tsx
@@ -1,15 +1,18 @@
 'use client'
-import { describe } from 'node:test'
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 
-export default function TaskInput() {
+type submiteType = {
+    submite: (task: { taskTitle: string, taskDes: string }) => void
+}
 
-    type formdata = {
+export default function TaskInput({ submite }: submiteType) {
+
+    type formDataType = {
         title: string,
         description: string
     }
 
-    const [formdata, setFormdata] = useState<formdata>({
+    const [formdata, setFormdata] = useState<formDataType>({
         title: '',
         description: ''
     })
@@ -21,12 +24,17 @@ export default function TaskInput() {
         }))
     }
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        submite({ taskTitle: formdata.title, taskDes: formdata.description })
+    }
 
     return (
+
         <div className='w-full bg-gray-200 rounded-2xl'>
             <div className='w-full h-full p-10 flex flex-col items-center justify-center gap-6'>
                 <p className='text-4xl font-semibold text-blue-500'>Add New Task</p>
-                <form className='w-full flex flex-col gap-6 items-center justify-center'>
+                <form className='w-full flex flex-col gap-6 items-center justify-center' onSubmit={handleSubmit}>
                     <div className='w-full max-w-md'>
                         <label htmlFor="tasktitle" className='text-xl font-normal'>Task Title :</label>
                         <input onChange={handleChange} type="text" name='tasktitle' id='tasktitle' placeholder='Task Title' className='p-2 bg-gray-300 rounded-lg w-full' />
@@ -36,7 +44,7 @@ export default function TaskInput() {
                         <input type="text" name='taskdes' id='taskdes' placeholder='Task Description' className='p-2 bg-gray-300 rounded-lg w-full' />
                     </div>
                     <div className='w-full max-w-md'>
-                        <button onSubmit={handleSubmit} className='p-2 bg-blue-400 rounded-lg w-full'>Add Task</button>
+                        <button className='p-2 bg-blue-400 rounded-lg w-full'>Add Task</button>
                     </div>
                 </form>
             </div>
