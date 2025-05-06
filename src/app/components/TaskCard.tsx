4f57b6735d317ab75c2db234f955e40cefdf1995
sujal_01 <sujalpatelyo1042@gmail.com>
2025-05-06 18:28:32 +0530

    updated task feature added

--- src/app/components/TaskCard.tsx
+++ src/app/components/TaskCard.tsx
@@ -8,7 +8,7 @@ type taskType = {
     _id?: string
 }
 
-export default function TaskCard({ task }: { task: taskType }) {
+export default function TaskCard({ task, click }: { task: taskType, click: (taskTitle: string, taskDes: string, taskId: string) => void }) {
 
     function timeAgo(dateString: string): string {
         const date = new Date(dateString);
@@ -30,12 +30,21 @@ export default function TaskCard({ task }: { task: taskType }) {
         return years === 1 ? '1 year ago' : `${years} years ago`;
     }
 
+    const handleEdit = () => {
+        if (task._id) {
+            click(task.taskTitle, task.taskDes, task._id)
+        }
+    }
+
     return (
         <div className='mt-20 w-full     max-w-sm'>
             <div className='px-6 py-6 bg-gray-300 rounded-2xl'>
                 <p className='text-2xl text-zinc-800'>{task.taskTitle}</p>
                 <p className='text-lg text-zinc-500'>{task.taskDes}</p>
-                <button className='mt-4 px-3 py-2 bg-blue-500 rounded-lg text-gray-100 cursor-pointer'>Done</button>
+                <div className='flex gap-2'>
+                    <button className='mt-4 px-3 py-2 bg-blue-500 rounded-lg text-gray-100 cursor-pointer'>Done</button>
+                    <button className='mt-4 px-3 py-2 bg-blue-500 rounded-lg text-gray-100 cursor-pointer' onClick={handleEdit}>Edit</button>
+                </div>
                 <p className='mt-4'>{timeAgo(task.taskDate)}</p>
             </div>
         </div>
