cf659990843bdd3672e8fc8cc43784deb4bfee67
sujal_01 <sujalpatelyo1042@gmail.com>
2025-05-07 14:43:38 +0530

    return if already connected to mongo

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDb = () => {
-    try {
-        mongoose.connect('mongodb://localhost:27017/TaskManager').then(() => {
-            console.log('MongoDb Connnected Successfully');
-        }).catch((error) => {
-            console.log("Error While Connecting To MongoDb", error);
-        })
-    } catch (err) {
-        console.log('Error In Mongoose Connection : ', err)
+    if (mongoose.connection.readyState === 1) {
+        return
+    } else {
+        try {
+            mongoose.connect('mongodb://localhost:27017/TaskManager').then(() => {
+                console.log('MongoDb Connnected Successfully');
+            }).catch((error) => {
+                console.log("Error While Connecting To MongoDb", error);
+            })
+        } catch (err) {
+            console.log('Error In Mongoose Connection : ', err)
+        }
     }
 }
 
